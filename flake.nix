2093248643f8387da2a564fd8b3201275b5514cb
Maxim <maxim@andromenet.org>
2024-12-03 23:02:31 +0100

    Set up basic Python environment
    Python environment with common scientific packages.
    Default package to combine all specified environments for use with `nix shell`.

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    lib = pkgs.lib;
+    python = pkgs.python312;
+  in {
+
+    environments.${system} = {
+      python = python.withPackages(pythonPackages: with pythonPackages; [
+        numpy
+        scipy
+        matplotlib
+        pandas
+        pint
+        astropy
+        sympy
+        jupyter
+      ]);
+    };
+
+    packages.${system}.default = pkgs.buildEnv {
+      name = "science";
+      paths = builtins.attrValues self.environments.${system};
+    };
+
+  };
+}
